9a0e8955dcaee84c402db65e2b3314973a650e71
Timofey Kononov <timofey.kononov@mail.ru>
2025-03-03 17:57:25 +0300

    Link-FIX

--- backend/foodgram/views.py
+++ backend/foodgram/views.py
@@ -8,4 +8,4 @@ def short_link_redirect(request, short_code):
     Перенаправление с короткой ссылки на оригинальный URL.
     """
     short_link = get_object_or_404(ShortLink, short_code=short_code)
-    return redirect(f'http://localhost/recipes/{short_link.original_url}')
+    return redirect(f'http://158.160.6.136:8000/recipes/{short_link.original_url}')
