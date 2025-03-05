b2c8bc9479c167520fbe732e8fc75c69f583062b
Timofey Kononov <timofey.kononov@mail.ru>
2025-03-05 15:57:14 +0300

    Urls replace

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,13 +1,62 @@
 from django.urls import include, path
+from djoser.views import UserViewSet
 from rest_framework import routers
 
-from api.views import IngredientViewSet, RecipeViewSet, TagViewSet
-from user.urls import user_urlpatterns
+from api.views import (
+    CustomUserViewSet,
+    IngredientViewSet,
+    RecipeViewSet,
+    ListUserViewSet,
+    TagViewSet
+)
+
 
 router_v1 = routers.DefaultRouter()
 router_v1.register('recipes', RecipeViewSet)
 router_v1.register('tags', TagViewSet)
 router_v1.register('ingredients', IngredientViewSet)
+
+user_urlpatterns = [
+    path(
+        'users/',
+        ListUserViewSet.as_view({'get': 'list', 'post': 'create', }),
+        name='user-create'
+    ),
+    path(
+        'users/subscriptions/',
+        CustomUserViewSet.as_view({'get': 'subscriptions'}),
+        name='user-subscriptions'
+    ),
+    path(
+        'users/<int:id>/',
+        UserViewSet.as_view({'get': 'retrieve'}),
+        name='user-detail'
+    ),
+    path(
+        'users/<int:id>/subscribe/',
+        CustomUserViewSet.as_view(
+            {'post': 'subscribe', 'delete': 'unsubscribe'}
+        ),
+        name='user-subscribe'
+    ),
+    path(
+        'users/me/',
+        CustomUserViewSet.as_view({'get': 'me'}),
+        name='user_me'
+    ),
+    path(
+        'users/me/avatar/',
+        CustomUserViewSet.as_view(
+            {'put': 'update_avatar', 'delete': 'update_avatar'}
+        ),
+        name='user_me_avatar'
+    ),
+    path(
+        'users/set_password/',
+        UserViewSet.as_view({'post': 'set_password'}),
+        name='set-password'
+    ),
+]
 
 v1_patterns = [
     path('', include(router_v1.urls)),
